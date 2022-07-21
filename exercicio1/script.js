3f38a2245a948905b7bb30e202a85e9336d49837
Alesouza7 <alexcostasouza86@yahoo.com>
2022-07-21 20:33:32 -0300

    Novas atualizações

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,7 @@
 //a) Peça ao usuário que insira um número **par**
 
 let numeroPar = Number(prompt("Digite um numero par?"))
-console.log(numeroPar)
+console.log("Numero digitado",numeroPar)
 
 //b) Imprima no console **o resto da divisão** desse número por 2.
 
@@ -9,8 +9,8 @@ Number(console.log("o resto da divisão",numeroPar%2))
 
 //c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
 
-//Indenpendente do numero sempre o resto da divisão tera o resultado zero desde que o numero seja sempre par.
+console.log("Indenpendente do numero sempre o resto da divisão tera o resultado zero desde que o numero seja sempre par.")
 
 //d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código
 
-//Se for digitado um impar qualquer o resto da divisão sempre será 1.
+console.log("Se for digitado um numero impar sempre o resto da divisão será 1.")
