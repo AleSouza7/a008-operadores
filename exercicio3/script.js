94351e5dc13603924d9ca604f36a72cdddfcae85
Alesouza7 <alexcostasouza86@yahoo.com>
2022-07-21 17:38:15 -0300

    Exercicio3 concluido

---
+++ exercicio3/script.js
@@ -0,0 +1,16 @@
+//# Exercício 3
+
+//Escreva as operações lógicas abaixo em formato de código. 
+//Imprima no console se o valor da operação é verdadeira ou falsa:
+
+//a) 5 é maior que 20 e também é menor que 2;
+typeof console.log("a) 5 é maior que 20 e também é menor que 2?",5 > 20 && 2)
+
+//b) 5 é igual a 5 ou é igual a “5”;
+typeof console.log("b) 5 é igual a 5 ou é igual a “5”?",5 == 5 || "5")
+
+//c) negação de vinte é maior que cinquenta
+typeof console.log ("c) negação de vinte é maior que cinquenta?",!20 > 50)
+
+//d) negação de (vinte é maior que cinquenta)
+typeof console.log ("d) negação de (vinte é maior que cinquenta)?",!(20 > 50))
